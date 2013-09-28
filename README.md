03716309602cc84683adb20b7e48ab4b4098af08
Richard Castera <richard.castera@gmail.com>
2013-09-28 17:19:58 -0400

    Update for psr-0 and add composer

--- README.md
+++ README.md
@@ -7,41 +7,129 @@ from scratch to achieve certain functionality. This class's inception, is the fr
 those instances. With this class you can easily time-out your authenticated users after 
 a specified interval.
 
+### Setup
+-----------------
+ Add a `composer.json` file to your project:
 
-Examples
------------
-    From within your app, once authenticated, you would run this to check if the user's session is still valid.
-    <?php
-      // Load the class and instatiate it.
-      require_once('Class.Session.php');
-      $Session = new Session(30); // Measured in minutes; this case 30
+```javascript
+{
+  "require": {
+      "rcastera/session": "v1.0.0"
+  }
+}
+```
 
-      // Check if the session registered.
-      if ($Session->isRegistered()) {
+Then provided you have [composer](http://getcomposer.org) installed, you can run the following command:
+
+```bash
+$ composer.phar install
+```
+
+That will fetch the library and its dependencies inside your vendor folder. Then you can add the following to your
+.php files in order to use the library (if you don't already have one).
+
+```php
+require 'vendor/autoload.php';
+```
+
+Then you need to `use` the relevant class, and instantiate the class. For example:
+
+
+### Getting Started
+-----------------
+```php
+require 'vendor/autoload.php';
+
+use rcastera\Browser\Session\Session;
+
+$session = new Session();
+```
+
+
+### Examples
+-----------------
+
+##### Logging in. (login.php)
+```php
+<?php
+    require 'vendor/autoload.php';
+    use rcastera\Browser\Session\Session;
+
+    $errors = array();
+
+    // You'll definitely want to add more validation here and check against a
+    // database or something. This is just an example.
+    if (! empty($_POST)) {
+        if ($_POST['username'] == 'test' && $_POST['password'] == 'test') {
+            $session = new Session();
+
+            // You can define what you like to be stored.
+            $user = array(
+                'user_id' => 1,
+                'username' => $_POST['username']
+            );
+
+            $session->register(120); // Register for 2 hours.
+            $session->set('current_user', $user);
+            header('location: index.php');
+            exit;
+        } else {
+            $errors[] = 'Invalid login.';
+        }
+    }
+?>
+
+// Your form here.
+```
+
+
+##### Secure area once authenticated. (index.php/controller/whatever)
+```php
+<?php
+    require 'vendor/autoload.php';
+    use rcastera\Browser\Session\Session;
+
+    $session = new Session();
+
+    // Check if the session registered.
+    if ($session->isRegistered()) {
         // Check to see if the session has expired.
         // If it has, end the session and redirect to login.
-        if ($Session->isExpired()) {
-            $Session->end();
+        if ($session->isExpired()) {
+            $session->end();
             header('location: login.php');
             exit;
-        } 
-        else {
-          // Keep renewing the session as long as they keep taking action.
-          $Session->renew();
+        } else {
+            // Keep renewing the session as long as they keep taking action.
+            $session->renew();
         }
-      }
-      else {
+    } else {
         header('location: login.php');
         exit;
-      }
-    ?>
+    }
+?>
+```
 
-Contributing
-------------
 
+##### Logging out. (logout.php)
+```php
+<?php
+    require 'vendor/autoload.php';
+    use rcastera\Browser\Session\Session;
+
+    $session = new Session();
+    $session->end();
+    header('location: login.php');
+    exit;
+?>
+```
+
+
+### Contributing
+-----------------
 1. Fork it.
 2. Create a branch (`git checkout -b my_branch`)
 3. Commit your changes (`git commit -am "Added something"`)
 4. Push to the branch (`git push origin my_branch`)
-5. Create an [Issue][1] with a link to your branch
+5. Create an Issue with a link to your branch
 6. Enjoy a refreshing Coke and wait
---
+++ src/rcastera/Browser/Session/Session.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Copyright (c) 2010 Richard Castera
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without li`ation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+namespace rcastera\Browser\Session;
+
+class Session
+{
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        session_start();
+    }
+
+    /**
+     * Destructor.
+     */
+    public function __destruct()
+    {
+        unset($this);
+    }
+
+    /**
+     * Register the session.
+     *
+     * @param integer $time.
+     */
+    public function register($time = 60)
+    {
+        $_SESSION['session_id'] = session_id();
+        $_SESSION['session_start'] = $this->newTime();
+        $_SESSION['session_time'] = intval($time);
+    }
+
+    /**
+     * Checks to see if the session is registered.
+     *
+     * @return  True if it is, False if not.
+     */
+    public function isRegistered()
+    {
+        if (! empty($_SESSION['session_id'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Set key/value in session.
+     *
+     * @param mixed $key
+     * @param mixed $value
+     */
+    public function set($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Retrieve value stored in session by key.
+     *
+     * @var mixed
+     */
+    public function get($key)
+    {
+        return $_SESSION[$key];
+    }
+
+    /**
+     * Retrieve the global session variable.
+     *
+     * @return array
+     */
+    public function getSession()
+    {
+        return $_SESSION;
+    }
+
+    /**
+     * Gets the id for the current session.
+     *
+     * @return integer - session id
+     */
+    public function getSessionId()
+    {
+        return $_SESSION['session_id'];
+    }
+
+    /**
+     * Checks to see if the session is over based on the amount of time given.
+     *
+     * @return boolean
+    */
+    public function isExpired()
+    {
+        if ($_SESSION['session_start'] < $this->timeNow()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Renews the session when the given time is not up and there is activity on the site.
+     */
+    public function renew()
+    {
+        $_SESSION['session_start'] = $this->newTime();
+    }
+
+    /**
+     * Returns the current time.
+     *
+     * @return unix timestamp
+     */
+    private function timeNow()
+    {
+        $currentHour = date('H');
+        $currentMin = date('i');
+        $currentSec = date('s');
+        $currentMon = date('m');
+        $currentDay = date('d');
+        $currentYear = date('y');
+        return mktime($currentHour, $currentMin, $currentSec, $currentMon, $currentDay, $currentYear);
+    }
+
+    /**
+     * Generates new time.
+     *
+     * @return unix timestamp
+     */
+    private function newTime()
+    {
+        $currentHour = date('H');
+        $currentMin = date('i');
+        $currentSec = date('s');
+        $currentMon = date('m');
+        $currentDay = date('d');
+        $currentYear = date('y');
+        return mktime($currentHour, ($currentMin + $_SESSION['session_time']), $currentSec, $currentMon, $currentDay, $currentYear);
+    }
+
+    /**
+     * Destroys the session.
+     */
+    public function end()
+    {
+        session_destroy();
+        $_SESSION = array();
+    }
+}
